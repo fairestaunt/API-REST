64b2614a7d1e2b4e4015db2f1f999ac7ad77c30c
Davi <davi_freitasoliveira@hotmail.com>
2022-11-23 18:52:07 -0300

    Token Web

--- routes/cliente.js
+++ routes/cliente.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res, next) =>{
     res.status(200).send({
@@ -11,25 +13,52 @@ router.get('/', (req, res, next) =>{
 router.post('/cadastro', (req, res, next) =>{
     mysql.getConnection((error, conn) =>{
         if(error){return res.status(500).send({error: error})}
-        conn.query(
-            'INSERT INTO cliente (email, senha, token, user) VALUES (?,?,?,?)',
-            [req.body.email, req.body.password, req.body.token, req.body.user],
-            (error, resultado, field) =>{
+
+        conn.query('SELECT * FROM login WHERE email = ?',
+            [req.body.email],
+            (error, results) => {
                 conn.release();
-
-                if(error){
-                    return res.status(500).send({
-                        error: error,
-                        response: null
-                    })
+                if(results.length > 0){
+                    res.status(409).send({ mensagem: 'Email já cadastrado'})
                 }
+                else{
+                    conn.query('SELECT * FROM login WHERE user = ?',
+                        [req.body.user],
+                        (error, results) => {
+                            conn.release();
+                            if(results.length > 0){
+                                res.status(409).send({ mensagem: 'Usuário já cadastrado'})
+                            }
+                            else{
 
-                res.status(201).send({
-                    mensagem: 'CRIADO CLIENTE',
-                    id: resultado.insertId
-                });
+                                bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
+                                    if(errBcrypt) { return res.status(500).send({ error : errBcrypt})}
+                                    conn.query(
+                                        'INSERT INTO login (email, senha, user) VALUES (?,?,?)',
+                                        [req.body.email, hash, req.body.user],
+                                        (error, results, field) =>{
+                                            conn.release();
+                            
+                                            if(error){
+                                                return res.status(500).send({
+                                                    error: error,
+                                                    response: null
+                                                })
+                                            }
+                            
+                                            res.status(201).send({
+                                                mensagem: 'Usuário cadastrado com sucesso',
+                                                
+                                                //id: resultado.insertId
+                                            });
+                                        })
+                                });
+
+                            }
+                    });
+                }
             }
-        )
+        );
     });
 });
 
@@ -40,7 +69,7 @@ router.get('/consulta/:emailUser/:password', (req, res, next) =>{
         if(error){return res.status(500).send({error: error})}
 
         conn.query(
-            'SELECT * FROM cliente WHERE email = ? AND senha = ? OR user = ? AND senha = ?',
+            'SELECT * FROM login WHERE email = ? AND senha = ? OR user = ? AND senha = ?',
             [req.params.emailUser, req.params.password, req.params.emailUser, req.params.password],
             (error, resultado, field) =>{
                 conn.release();
@@ -64,6 +93,87 @@ router.get('/consulta/:emailUser/:password', (req, res, next) =>{
                 });
             }
         )
+    });
+});
+
+
+
+router.get('/consultaToken/:tabela/:token', (req, res, next) =>{
+    mysql.getConnection((error, conn) =>{
+        if(error){return res.status(500).send({error: error})}
+
+        conn.query(
+            'SELECT * FROM ?? WHERE token = ?',
+            [req.params.tabela, req.params.token],
+            (error, resultado, field) =>{
+                conn.release();
+
+                if(error){
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    })
+                }
+                let obj = undefined;
+
+                if(resultado.length > 0){
+                    if(!resultado[0].senha){
+                        obj = resultado[0];
+                    }
+                    else{
+                        obj = JSON.parse("{}");
+                        obj.user = resultado[0].user;
+                        obj.email = resultado[0].email;
+                    }   
+                }
+
+                return res.status(200).send({
+                    mensagem: 'ENCONTRADO CLIENTE',
+                    result: obj
+                });
+            }
+        )
+    });
+});
+
+
+
+
+router.post('/login', (req, res, next)=>{
+    mysql.getConnection((error, conn) =>{
+        if(error){return res.status(500).send({error: error})}
+        conn.query('SELECT * FROM login WHERE email = ? or user = ?',
+            [req.body.emailUser, req.body.emailUser],
+            (error, results, fields) => {
+                conn.release();
+                if(error){return res.status(500).send({error: error})}
+                if(results.length < 1){
+                    return res.status(401).send({ mensagem: 'Falha na autenticação'})
+                }
+                bcrypt.compare(req.body.password, results[0].senha, (err, result) => {
+                    if(err){
+                        return res.status(401).send({ mensagem : 'Falha na autenticação'})
+                    }
+
+                    if(result){
+                        const tokenWeb = jwt.sign({
+                            id_usuario: results[0].id_usuario,
+                            email: results[0].email
+                        }, 
+                        '${process.env.JWT_KEY}', 
+                        {
+                            expiresIn: "1h"
+                        });
+
+                        return res.status(200).send({
+                            mensagem: 'Autenticado com sucesso',
+                            token: tokenWeb
+                        })
+                    }
+
+                    return res.status(401).send({ mensagem : 'Falha na autenticação'})
+                });
+        });
     });
 });
 
